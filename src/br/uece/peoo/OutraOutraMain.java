f343a931cd2723ee831719f918bad3fd35b5ad23
ericson <moreira.ericson@gmail.com>
2020-03-26 13:50:50 -0300

    Item 2 concluido.

--- src/br/uece/peoo/OutraOutraMain.java
+++ src/br/uece/peoo/OutraOutraMain.java
@@ -3,6 +3,7 @@ package br.uece.peoo;
 import br.uece.peoo.model.Comida;
 import br.uece.peoo.model.Entidade;
 import br.uece.peoo.model.Robo;
+import br.uece.peoo.model.RoboInteligente;
 import br.uece.peoo.view.DisputaRobosPainel;
 import br.uece.peoo.view.RoboPanel;
 
@@ -11,16 +12,17 @@ import java.awt.*;
 import java.util.Scanner;
 
 /**
- * Instancie dois robôs, peça ao usuário para entrar com a posição do alimento, e faça os dois robôs se moverem
- * randomicamente, um de cada vez, até que um deles encontre o alimento. Ao final, mostre quem achou o alimento
- * e o número de movimentos que cada robô fez.
+ * Crie uma subclasse ReboInteligente que sobrescreve o método mover de forma que se robô fez um movimento inválido,
+ * garanta que o próximo movimento será válido. Cria uma classe Main que instancie um robô normal e outro inteligente,
+ * peça ao usuário para entrar com a posição do alimento, e faça os dois robôs se moverem randomicamente, um de
+ * cada vez, até que um deles encontre o alimento. Ao final, mostre quem achou o alimento e o número de movimentos
+ * que cada robô fez.
  */
-public class OutraClasseMain {
+public class OutraOutraMain {
 
     public static void main(String[] args) {
-
-        Robo azul = new Robo(Color.BLUE);
-        Robo verde = new Robo(Color.GREEN);
+        Robo azul = new Robo(Color.BLUE); // Robo normal
+        Robo verde = new RoboInteligente(Color.GREEN); // Robo Inteligente
 
         // Posição da Comida
         int x, y;
@@ -43,7 +45,5 @@ public class OutraClasseMain {
         frame.setPreferredSize(new Dimension(RoboPanel.WIDTH + 10, RoboPanel.WIDTH + 10));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
     }
-
 }
---
+++ src/br/uece/peoo/model/RoboInteligente.java
@@ -0,0 +1,31 @@
+package br.uece.peoo.model;
+
+import br.uece.peoo.exceptions.MovimentoInvalidoException;
+
+import java.awt.*;
+
+public class RoboInteligente extends Robo{
+
+    /**
+     * Construtor que recebe a cor do robô e o inicialize na posição (0,0)
+     *
+     * @param cor
+     */
+    public RoboInteligente(Color cor) {
+        super(cor);
+    }
+
+    /**
+     * Tenta mover. Caso aconteça um erro, tenta novamente recursicamente.
+     * Uma hora vai acertar o movimento.
+     * @param direction Recebe como parâmetro uma String e altera a posição do robô.
+     */
+    @Override
+    public void mover(String direction) throws MovimentoInvalidoException {
+        try {
+            super.mover(direction); // mover da classe mãe.
+        } catch (MovimentoInvalidoException e) {
+            moverRandom(); // mover do Robo Inteligente. Chamada recursiva.
+        }
+    }
+}
